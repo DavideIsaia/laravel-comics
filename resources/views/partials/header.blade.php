c1355d002806459efa322ef0af9de0deed9bdf59
DavideIsaia <davide.enjoy@hotmail.it>
2022-06-27 18:11:15 +0200

    sistemata line height anchor tags header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
   <img src="{{ asset('images/dc-logo.png') }}" alt="DC Logo" />
   <nav>
     @foreach($navbar as $key => $item)
-      <li class="{{ Request::route()->getName() === $navbar[$key] ? 'active' : '' }}"><a href="{{route($key)}}"> {{$item}} </a></li>
+      <li class="{{ Request::route()->getName() == $key ? 'active' : '' }}"><a href="{{route($key)}}"> {{$item}} </a></li>
     @endforeach
   </nav>
 </header>
